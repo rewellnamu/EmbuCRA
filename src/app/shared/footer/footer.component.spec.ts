6bf051752a78429352b0c2ec5640671f9252d3a7
rewell namu <riwenamu@gmail.com>
2025-10-07 11:12:17 +0300

    Import ActivatedRoute from @angular/router Import of from rxjs Add providers array with a mock ActivatedRoute in the TestBed.configureTestingModule()

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
@@ -8,7 +9,16 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
+      imports: [FooterComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { params: {} }
+          }
+        }
+      ]
     })
     .compileComponents();
 
--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -8,7 +9,16 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { params: {} }
+          }
+        }
+      ]
     })
     .compileComponents();
 
