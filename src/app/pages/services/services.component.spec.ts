61d298806f419f1d785b14c881d84a0f4e0ced7c
rewell namu <riwenamu@gmail.com>
2025-10-16 06:54:46 +0300

    fIXED service component failure test CI/CD

--- src/app/pages/services/services.component.spec.ts
+++ src/app/pages/services/services.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { ServicesComponent } from './services.component';
 
@@ -8,7 +10,16 @@ describe('ServicesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ServicesComponent]
+      imports: [ServicesComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: { params: {} }
+          }
+        }
+      ]
     })
     .compileComponents();
 
