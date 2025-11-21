6a9eee4c995e67557d63b2745bd2f9b3d6ab3c65
rewell namu <riwenamu@gmail.com>
2025-11-21 13:19:31 +0300

    security env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,9 +2,7 @@ export const environment = {
   production: false,
   admin: {
     // Hashed credentials (SHA-256)
-    // Original username: 'embucra'
     usernameHash: 'bee502253f2f755248b59894497451bf986fabd464a7577d33a54f7f9c7f6d14',
-    // Original password: 'ecra2025'
     passwordHash: 'a0fb0d3b75ac917a71eac5b1596b20a3de81d32591b882ec58dfc49811f5b327'
   }
 };
