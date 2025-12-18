d754a3ff4509e0494a7f6aef1cef11950da14545
rewell namu <riwenamu@gmail.com>
2025-12-18 10:41:56 +0300

    config improvements

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { provideTransloco, TranslocoLoader } from '@jsverse/transloco';
 import { routes } from './app.routes';
