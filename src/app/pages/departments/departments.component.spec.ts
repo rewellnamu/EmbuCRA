9b36dbaeacfe6df0121c546c95ec0f27a128674a
rewell namu <riwenamu@gmail.com>
2025-12-26 17:25:10 +0300

    CI/CD

--- src/app/pages/departments/departments.component.spec.ts
+++ src/app/pages/departments/departments.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DepartmentsComponent } from './departments.component';
+import { TranslocoService, TranslocoTestingModule } from '@jsverse/transloco';
 
 describe('DepartmentsComponent', () => {
   let component: DepartmentsComponent;
   let fixture: ComponentFixture<DepartmentsComponent>;
+  let translocoService: TranslocoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DepartmentsComponent]
-    })
-    .compileComponents();
+      imports: [
+        DepartmentsComponent,
+        TranslocoTestingModule.forRoot({
+          langs: {
+            en: {
+              departments: {
+                header: {
+                  title: 'Departments'
+                }
+              }
+            }
+          }
+        })
+      ]
+    }).compileComponents();
+
+    translocoService = TestBed.inject(TranslocoService);
+    translocoService.setActiveLang('en');
 
     fixture = TestBed.createComponent(DepartmentsComponent);
     component = fixture.componentInstance;
